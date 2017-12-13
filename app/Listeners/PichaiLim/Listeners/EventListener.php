d63dc4c67ad83e5568a9643c287a208c866e0487
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-12-14 02:50:35 +0700

    commit laravel

---
+++ app/Listeners/PichaiLim/Listeners/EventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners\PichaiLim\Listeners;
+
+use App\Events\PichaiLim\Events\Event;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EventListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function handle(Event $event)
+    {
+        //
+    }
+}
