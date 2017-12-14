0595b26da7b3c45eedae5ef4533deb435ad6a0b3
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-12-14 17:57:13 +0700

    commit

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Pichai Limpanitivat'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
