78d2978299f810db0390ef8fe1660b80945067d1
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-12-18 14:17:22 +0700

    create migration Profiles

---
+++ app/Profiles.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pichai;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profiles extends Model
+{
+    //
+
+    protected $fillable = [
+        'image','chapter', 'last_year', 'list_index', 'status_show'
+    ];
+}
---
+++ database/migrations/2017_12_18_070138_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');
+            $table->string('chapter',255);
+            $table->tinyInteger('last_year',4);
+            $table->integer('list_index')->index();
+            $table->enum('status_show',['on','off'])->default('on');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
